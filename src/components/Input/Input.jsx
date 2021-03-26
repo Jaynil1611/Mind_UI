fb0fe128acd8c9c14627155ef226f4f5624bd350
Jaynil1611 <48921037+Jaynil1611@users.noreply.github.com>
2021-03-26 17:45:38 +0530

    refactor: input component

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -9,20 +9,20 @@ function Input(props) {
       <h3> Usage </h3>
       <h3> Input Textbox </h3>
       <div className="input-examples">
-        <div className="input-box">
-          <label className="input-label">Name</label>
+        <div className="input__box">
+          <label className="input__label">Name</label>
           <input className="input" type="text" />
-          <p className="input-error"> Full Name should be written*</p>
+          <p className="input--error"> Full Name should be written*</p>
         </div>
-        <div className="input-box">
-          <label className="input-label"> Email </label>
+        <div className="input__box">
+          <label className="input__label"> Email </label>
           <input className="input" type="email" />
-          <p className="input-error"> Please enter a valid email*</p>
+          <p className="input--error"> Please enter a valid email*</p>
         </div>
-        <div className="input-box">
-          <label className="input-label"> Password </label>
+        <div className="input__box">
+          <label className="input__label"> Password </label>
           <input className="input" type="password" />
-          <p className="input-error"> Password should be 6 chars long*</p>
+          <p className="input--error"> Password should be 6 chars long*</p>
         </div>
       </div>
     </div>
