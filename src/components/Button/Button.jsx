bc6e087b77d60aa0c23803c290c390d80784b3c9
Jaynil1611 <48921037+Jaynil1611@users.noreply.github.com>
2021-03-26 12:56:56 +0530

    refactor: button  component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -12,36 +12,36 @@ function Button(props) {
       <h3> Usage </h3>
       <h3> Primary Button </h3>
       <div className="button-examples">
-        <button type="button" className="button button-primary button-sm">
+        <button type="button" className="button button--primary button--sm">
           Button
         </button>
-        <button type="button" className="button button-primary button-md">
+        <button type="button" className="button button--primary button--md">
           Button
         </button>
-        <button type="button" className="button button-primary">
+        <button type="button" className="button button--primary">
           Button
         </button>
         <button
           type="button"
-          className="button button-primary button-lg button-link"
+          className="button button--primary button--lg button__link"
         >
           <a href="https://github.com/Jaynil1611/mind-ui"> Button </a>
         </button>
       </div>
       <h3> Secondary Button </h3>
       <div className="button-examples">
-        <button type="button" className="button button-secondary button-sm">
+        <button type="button" className="button button--secondary button--sm">
           Button
         </button>
-        <button type="button" className="button button-secondary button-md">
+        <button type="button" className="button button--secondary button--md">
           Button
         </button>
-        <button type="button" className="button button-secondary">
+        <button type="button" className="button button--secondary">
           Button
         </button>
         <button
           type="button"
-          className="button button-secondary button-lg button-link"
+          className="button button--secondary button--lg button__link"
         >
           <a href="https://github.com/Jaynil1611/mind-ui"> Button </a>
         </button>
@@ -49,25 +49,25 @@ function Button(props) {
       <h3> Icon Button </h3>
       <p> Icons can be used as buttons to perform certain actions.</p>
       <div className="button-examples">
-        <button className="button button-icon">
+        <button className="button button__icon">
           <i className="fas fa-trash-alt fa-2x"></i>
         </button>
-        <button type="button" className="button button-icon">
+        <button type="button" className="button button__icon">
           <i className="fas fa-bell fa-2x"></i>
         </button>
-        <button type="button" className="button button-icon">
+        <button type="button" className="button button__icon">
           <i className="fas fa-play fa-2x"></i>
         </button>
       </div>
       <h3> Floating Action Button </h3>
       <div className="button-examples">
-        <button type="button" className="button-floating">
+        <button type="button" className="button__action">
           +
         </button>
-        <button type="button" className="button-floating">
+        <button type="button" className="button__action">
           <i className="fas fa-times"></i>
         </button>
-        <button type="button" className="button-floating">
+        <button type="button" className="button__action">
           <i className="fas fa-pen"></i>
         </button>
       </div>
